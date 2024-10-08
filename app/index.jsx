476e08e527ae9095e535221fdebee42b73853360
Tyrion Lannister <win.pomerantz@gmail.com>
2024-10-08 16:04:22 +0200

    first commit, beginning file structure and styles added

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3x1 font-pblack">ReadyCheck: Not Ready</Text>
+      <StatusBar style="auto" />
+      <Link href='/profile' style={{ color: blue}}>Go to Profile</Link>
+    </View>
+  );
+}
