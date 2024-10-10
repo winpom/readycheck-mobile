930db31ec76fe789ed2174ad872048045d15932a
Tyrion Lannister <win.pomerantz@gmail.com>
2024-10-10 17:29:29 +0200

    coding break, saving progress on sign-up page -- incomplete

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3x1 font-pblack">ReadyCheck: Not Ready</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full" >
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text 3-l text-white font-bold text-center">
+              Get Ready for Anything with{' '}
+              <Text classname="text-secondary-200">ReadyCheck</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-2"
+              resizeMode="contain"
+            ></Image>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where seeing who's ready is as simple as a check
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
