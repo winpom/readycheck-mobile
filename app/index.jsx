efe121dca93453a9215c437457c33db64ce965f5
Tyrion Lannister <win.pomerantz@gmail.com>
2024-10-10 19:09:39 +0200

    fixed button issue on welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,29 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image, ScrollView } from 'react-native';
-import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image, ScrollView } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { images } from '../constants'
+import { images } from "../constants";
+import { CustomButton } from "../components";
+
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full" >
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-5">
           <Image
             source={images.logo}
-            className="w-[130px] h-[84px]"
+            className="w-[280px] h-[84px]"
             resizeMode="contain"
           />
           <Image
             source={images.cards}
-            className="max-w-[380px] w-full h-[300px]"
+            className="max-w-[380px] w-full h-[280px]"
             resizeMode="contain"
           />
           <View className="relative mt-5">
             <Text className="text 3-l text-white font-bold text-center">
-              Get Ready for Anything with{' '}
-              <Text classname="text-secondary-200">ReadyCheck</Text>
+              Get Ready for Anything with{" "}
+              <Text className="text-secondary-200">ReadyCheck</Text>
             </Text>
             <Image
               source={images.path}
