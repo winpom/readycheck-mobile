1186e95bb25e503bc9574503fa6e5dec7d75d409
Win <160787222+winpom@users.noreply.github.com>
2024-10-22 16:39:56 +0200

    added search function for users, made usercard for the results

--- app/(tabs)/social.jsx
+++ app/(tabs)/social.jsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import SearchInput from "../../components/SearchInput";
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 
 const Social = () => {
   return (
-    <View>
-      <Text>Social</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="text-white">Social</Text>
+      <SearchInput />
+    </SafeAreaView>
   )
 }
 
--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -1,16 +1,13 @@
 import { View, Text, Image } from 'react-native';
 import React from 'react';
 
-import { formatTiming } from '../utils/formatTiming';
 import { icons } from '../constants';
 
-const ReadyCheckCard = ({ readycheck }) => {
+const UserCard = ({ user }) => {
     const {
-        title = 'Untitled',
-        activity = 'Unknown Activity',
-        timing = 'Unknown Time',
-        owner: { username = 'Anonymous', avatar = icons.defaultAvatar } = {}, // Default values for owner
-    } = readycheck || {}; // Fallback to empty object if readycheck is undefined
+        username = 'Anonymous', 
+        avatar = icons.defaultAvatar // Default avatar if none provided
+    } = user || {}; // Fallback to an empty object if user is undefined
 
     return (
         <View className="flex-col items-center px-4 mb-14">
@@ -24,21 +21,10 @@ const ReadyCheckCard = ({ readycheck }) => {
                         />
                     </View>
                     <View className="justify-center flex-w ml-3 gap-y-1">
-                        <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
-                            {title}
-                        </Text>
                         <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
                             {username}
                         </Text>
                     </View>
-                </View>
-                <View className="justify-center flex-w ml-3 gap-y-1">
-                    <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
-                        {activity}
-                    </Text>
-                    <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
-                        {formatTiming(timing)}
-                    </Text>
                 </View>
                 <View className="pt-2">
                     <Image
@@ -52,4 +38,4 @@ const ReadyCheckCard = ({ readycheck }) => {
     );
 }
 
-export default ReadyCheckCard;
+export default UserCard;
