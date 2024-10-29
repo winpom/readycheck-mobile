2702ec8f4691997ee681b7d0402222b3894545c0
Win <160787222+winpom@users.noreply.github.com>
2024-10-29 12:48:58 +0100

    improving live readycheck page

--- app/(pages)/readycheck/[readycheckId].jsx
+++ app/(pages)/readycheck/[readycheckId].jsx
@@ -1,10 +1,12 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getReadyCheck } from "../../../lib/appwrite";
 import { formatTiming } from "../../../utils/formatTiming";
 import InfoBox from "../../../components/InfoBox";
+import UserCard from "../../../components/UserCard"
+
 
 const LiveReadyCheck = () => {
     const { readycheckId } = useLocalSearchParams();
@@ -33,15 +35,24 @@ const LiveReadyCheck = () => {
                 </TouchableOpacity>
             </View>
             <View className="px-4">
-                <InfoBox title={title} subtitle="ReadyCheck Title" />
-                <Text className="text-white text-lg my-2">Timing: {time} on {date}</Text>
-                <Text className="text-white text-lg my-2">Countdown: {Math.max(countdown / 1000, 0).toFixed(0)} seconds</Text>
+                <Text className="text-secondary text-3xl my-2 text-center">{title}</Text>
+                <Text className="text-white text-lg my-2 text-center">{Math.max(countdown / 1000, 0).toFixed(0)} seconds</Text>
+                <Text className="text-white text-lg my-2">Owner: {owner.username}</Text>
+                <Text className="text-white text-lg my-2">When: {time} on {date}</Text>
                 <Text className="text-white text-lg my-2">Description: {description}</Text>
-                <InfoBox title={owner.username} subtitle="Owner" />
                 <Text className="text-white text-lg my-2">Invitees:</Text>
-                {invitees.map((invitee, index) => (
-                    <InfoBox key={invitee.$id || index} title={invitee.username} subtitle="Invitee" />
-                ))}
+                <FlatList
+                    data={invitees}
+                    keyExtractor={(item) => item.id || item.$id} // Ensure a unique key, fallback to item.$id if necessary
+                    renderItem={({ item }) => <UserCard user={item} />}
+                    ListEmptyComponent={() => (
+                        <View className="justify-center items-center px-4">
+                            <Text className="text-lg text-center font-psemibold text-white mt-2">
+                                No Invites Sent
+                            </Text>
+                        </View>
+                    )}
+                />
             </View>
         </SafeAreaView>
     );
